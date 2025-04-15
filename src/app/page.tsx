7bea12e3ad99e3513711723fd9ad7fb3860b08a1
Romel Sedillo <romelsedillo@gmail.com>
2025-04-15 23:46:16 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import AnimeRecommendation from "@/components/layout/AnimeRecommendation";
-import UpComingSeason from "@/components/layout/UpComingSeason";
 
 export default function Home() {
   return (
     <div className="max-w-7xl flex flex-col mx-auto pt-0 bg-[#121212]">
       <AnimeRecommendation />
-      <UpComingSeason />
     </div>
   );
 }
