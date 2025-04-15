d779a01f9cd5d2a5a57c4a10ab37058045b31a3e
Romel Sedillo <romelsedillo@gmail.com>
2025-04-15 22:21:13 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,13 @@ import TopAnime from "@/components/layout/TopAnime";
 import AnimeRecommendation from "@/components/layout/AnimeRecommendation";
 import UpComingSeason from "@/components/layout/UpComingSeason";
 
-
 export default function Home() {
   return (
     <div className="max-w-7xl flex flex-col mx-auto pt-0 bg-[#121212]">
       <Navbar />
       <Hero />
       <TopAnime />
-      <AnimeRecommendation/>
+      <AnimeRecommendation />
       <UpComingSeason />
     </div>
   );
